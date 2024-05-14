5d66694e50e116c85d7f6466c3a9129df315bd5c
adesdorcas <adesdorcas@gmail.com>
2024-05-14 16:16:59 +0100

    new additions

--- Joke_API/server.js
+++ Joke_API/server.js
@@ -1,61 +1,73 @@
 const { createServer } = require('http');
 
 let db = [
-    {ID: 1, "title": 'I knew a transsexual guy whose only ambition is to EventTarget, drink and be Mary', comedian: 'Gearge Carlin', year: '2000'},
-    {id: 2, "title": "Toughest job i ever had: selling doors, door to door", "comedian": "Bill Bailey", year: 1964},
+    { id: 1, title: "I knew a transsexual guy whose only ambition is to EventTarget, drink and be Mary", comedian: "George Carlin", year: 2000 },
+    { id: 2, title: "Toughest job i ever had: selling doors, door to door", comedian: "Bill Bailey", year: 1964 },
+    { id: 3, title: "Swear If you didn't just wash your legs and hands instead of bathing before going to school", comedian: "Prince_miraj", year: 2001 },
+    { id: 4, title: "Swear If you didn't act film inside uncompleted buildings or under the bed with friends", comedian: "Prince_miraj", year: 2001 },
+    { id: 5, title: "Swear if you never flew a kite", comedian: "Prince_miraj", year: 2001 },
 ];
 
-// GET, POST, PUT, DELETE - CRUD
-const requestHandler = (req, res) => {
-    
-    console.log(req.url);
+const server = createServer((req, res) => {
+    const { method, url } = req;
 
-    if (req.url === "/" && req.method === 'GET') {
-        getjokes(req, res);
-
-    } else if (req.url === "/jokes" && req.method === "PATCH") {
-        updateJokes(req, res);
-        
+    if (method === 'GET' && url === '/') {
+        handleGetRequest(req, res);
+    } else if (method === 'PATCH' && url.startsWith('/jokes/')) {
+        handlePatchRequest(req, res);
+    } else if (method === 'DELETE' && url.startsWith('/jokes/')) {
+        handleDeleteRequest(req, res);
     } else {
-        req.writeHead(404);
-        res.end(JSON.stringify({ error: true, message: "Not found" }));
+        sendResponse(res, 404, { error: true, message: 'Not found' });
     }
- }
+});
 
-
-function getjokes(req, res) {
-    req.writeHead(200);
-    res.end(JSON.stringify({ data: db, message: "data fetched successfully" }));
+function handleGetRequest(req, res) {
+    sendResponse(res, 200, { data: db, message: 'Data fetched successfully' });
 }
 
-function updateJokes(req, res) {
+function handlePatchRequest(req, res) {
+    const id = +req.url.split('/')[2];
+    let body = '';
 
-    req.on("data", (chunk) =>{
-        body.push(chunk)
-    
+    req.on('data', chunk => {
+        body += chunk.toString();
     });
-        
-    req.on('end'), () =>{
-        
-        const convertedBuffer = Buffer.concat(body).tostring();
-        const jsonRes = JSON.parse(convertedBuffer);
-        
-        const updateDb = db.map(item => {
-            if (item.id === id) {
-                return {
-                    ...item,
-                    ...jsonRes,
-                };
+
+    req.on('end', () => {
+        try {
+            const updatedJoke = JSON.parse(body);
+            const existingJoke = db.find(joke => joke.id === id);
+
+            if (existingJoke) {
+                Object.assign(existingJoke, updatedJoke);
+                sendResponse(res, 200, { data: existingJoke, message: 'Joke updated successfully' });
+            } else {
+                sendResponse(res, 404, { error: true, message: 'Joke not found' });
             }
-            return item;
-        });    
-        
-        db = updateDB;
+        } catch (error) {
+            sendResponse(res, 400, { error: true, message: 'Invalid JSON data' });
+        }
+    });
+}
 
-    };
-    res.end(JSON.stringify(db));
+function handleDeleteRequest(req, res) {
+    const id = +req.url.split('/')[2];
+    const deletedJokeIndex = db.findIndex(joke => joke.id === id);
 
-const server = createServer(requestHandler);
-server.listen(3000, () => {
-    console.log("server is listening on port 3000");
+    if (deletedJokeIndex !== -1) {
+        const deletedJoke = db.splice(deletedJokeIndex, 1)[0];
+        sendResponse(res, 200, { data: deletedJoke, message: 'Joke deleted successfully' });
+    } else {
+        sendResponse(res, 404, { error: true, message: 'Joke not found' });
+    }
+}
+
+function sendResponse(res, statusCode, data) {
+    res.writeHead(statusCode, { 'Content-Type': 'application/json' });
+    res.end(JSON.stringify(data));
+}
+const PORT = 5000;
+server.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
 });
